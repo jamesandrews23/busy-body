c678d40549961102f6bd8c8506be3c525dced052
jamesandrews23 <james.andrews23@gmail.com>
2020-08-20 15:01:22 -0500

    Moves all rss feeds to FeedFeeder. Adds a catch to each feed.

--- src/App.js
+++ src/App.js
@@ -1,56 +1,9 @@
 import React from "react";
 import "./App.css";
-import axios from "axios";
 import _ from 'lodash';
 import convertXmlToJson from './Parser';
 import Main from './Main';
-
-const proxy = "https://cors-anywhere.herokuapp.com/";
-
-function getCnnFeed() {
-    return axios.get(proxy + 'http://rss.cnn.com/rss/cnn_topstories.rss');
-}
-
-function getBbcFeed() {
-    return axios.get(proxy + "http://feeds.bbci.co.uk/news/rss.xml#");
-}
-
-function getFoxFeed(){
-    return axios.get(proxy + "http://feeds.foxnews.com/foxnews/latest");
-}
-
-function getWsjFeed(){
-    return axios.get(proxy + "https://feeds.a.dj.com/rss/RSSWSJD.xml");
-}
-
-function getWeatherFeed(){
-    return axios.get(proxy + "https://www.weather.gov/headline_archive.php");
-}
-
-function getEspnFeed(){
-    return axios.get(proxy + "http://www.espn.com/espn/news/story?page=rssinfo");
-}
-
-function getLifeHackerFeed(){
-    return axios.get(proxy + "http://www.espn.com/espn/news/story?page=rssinfo");
-}
-
-function getNytFeed(){
-    return axios.get(proxy + "https://rss.nytimes.com/services/xml/rss/nyt/US.xml");
-}
-
-function getLocFeed(){
-    return axios.get(proxy + "http://www.loc.gov/rss/pao/news.xml");
-}
-
-function getAlgFeed(){
-    return axios.get(proxy + "https://www.feedspot.com/infiniterss.php?_src=followbtn&followfeedid=4275711&q=site:http%3A%2F%2Fwww.aljazeera.com%2Fxml%2Frss%2Fall.xml");
-}
-
-function getYahooFeed(){
-    return axios.get(proxy + "https://www.yahoo.com/news/rss/world");
-}
-
+import {getFeeds} from './FeedFeeder';
 
 let rssOriginal = [];
 
@@ -95,9 +48,8 @@ class App extends React.Component {
 
     componentDidMount(){
         var that = this;
-        Promise.all([getCnnFeed(), getBbcFeed(), getFoxFeed(), getWsjFeed(), getWeatherFeed(), getEspnFeed(),
-            getLifeHackerFeed(), getNytFeed(), getYahooFeed()])
-            .then(function (results) {
+        Promise.all(getFeeds())
+            .then((results) => {
                 let feeds = results.map(function(result) {
                     let objFeed = convertXmlToJson(result.data);
                     if(!_.isEmpty(objFeed) && objFeed["channel"]){
@@ -106,6 +58,9 @@ class App extends React.Component {
                 });
                 that.setState({rss: feeds});
                 rssOriginal = feeds;
+            })
+            .catch((error) => {
+                console.log(error);
             });
     }
 
---
+++ src/FeedFeeder.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+const proxy = "https://cors-anywhere.herokuapp.com/";
+
+class Feed {
+    constructor(id, path){
+        this.id = id;
+        this.path = path;
+    }
+
+    getPath(){
+        return this.path;
+    }
+
+    getId(){
+        return this.id;
+    }
+}
+
+const feeds = [new Feed('cnn', 'http://rss.cnn.com/rss/cnn_topstories.rss'),
+    new Feed('bbc', 'http://feeds.bbci.co.uk/news/rss.xml#'),
+    new Feed('fox', 'http://feeds.foxnews.com/foxnews/latest'),
+    new Feed('wsj', 'https://feeds.a.dj.com/rss/RSSWSJD.xml'),
+    new Feed('nyt', 'https://archive.nytimes.com/www.nytimes.com/services/xml/rss/index.html?mcubz=0'),
+    new Feed('yahoo', 'https://www.yahoo.com/news/rss/world')];
+
+function getFeeds(){
+    return feeds.map((feed) => {
+        return axios.get(proxy + feed.getPath()).catch(error => { console.log(error); });
+    });
+}
+
+function addFeed(id, path){
+    if(id && path){
+        feeds.push(new Feed(id, path));
+    } else {
+        console.error('invalid feed');
+    }
+}
+
+export {getFeeds, addFeed};
