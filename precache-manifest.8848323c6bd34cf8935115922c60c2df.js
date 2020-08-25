3af568005d4183e9e867f1117030b4b7b2d3fb8f
jamesandrews23 <james.andrews23@gmail.com>
2020-08-25 11:55:25 -0500

    Updates

---
+++ precache-manifest.8848323c6bd34cf8935115922c60c2df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1206707c92144a5946d7ef8c7a4209d7",
+    "url": "/busy-body/index.html"
+  },
+  {
+    "revision": "3d990c4425d6ab159224",
+    "url": "/busy-body/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "20ccb1ff03fbe8b25ec0",
+    "url": "/busy-body/static/js/2.5c91484c.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/busy-body/static/js/2.5c91484c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d990c4425d6ab159224",
+    "url": "/busy-body/static/js/main.d1bbadaa.chunk.js"
+  },
+  {
+    "revision": "129b1e2a24c0b56a1766",
+    "url": "/busy-body/static/js/runtime-main.ac1fde37.js"
+  }
+]);
