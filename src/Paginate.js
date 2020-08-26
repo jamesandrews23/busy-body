dfabc81ac1638e9ccb02e7f1e23b18918ba374da
jamesandrews23 <james.andrews23@gmail.com>
2020-08-26 12:42:41 -0500

    Checks if the list size is less than or equal to the max. Paginate was breaking search.

--- src/Paginate.js
+++ src/Paginate.js
@@ -1,6 +1,10 @@
 const max = 30;
 
 function paginate(list, pageSize, pageNumber) {
+    if(list.length <= max){
+        return list; //if it's not more than the max, no pages are necessary
+    }
+
     return list.slice((pageNumber - 1) * pageSize, pageNumber * pageSize);
 }
 
